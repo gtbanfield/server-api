34d0aac73d95fc47dd51d9f762e0361f396c0e09
Grant Banfield <gtbanfield@gmail.com>
2025-03-25 21:13:31 +0000

    test

--- src/main/java/com/example/actuatordemo/controller/ApiAController.java
+++ src/main/java/com/example/actuatordemo/controller/ApiAController.java
@@ -1,7 +1,10 @@
 package com.example.actuatordemo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,12 @@ public class ApiAController {
     @GetMapping("/data")
     public ResponseEntity<String> getData() {
         return ResponseEntity.ok("Hello I am the Data from Server - API 2!!");
+    }
+
+    @GetMapping(path = "test", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> headerDemo(@RequestHeader(name = "reg-channel-version") String header) {
+        System.out.println("Header reg-channel-version is : " + header);
+        String response = "Header reg-channel-version is : " + header;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
